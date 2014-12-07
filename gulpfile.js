f6027cd6ecf1da5a36719663ff211b4235b1d64d
Dave Lunny <dlunny@featurecreeptechnology.com>
2014-12-07 17:00:28 -0500

    setup test stuffs

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp 	= require('gulp'),
+	less 	= require('gulp-less'),
 	concat 	= require('gulp-concat'),
 	prfx	= require('gulp-autoprefixer'),
 	comb	= require('gulp-csscomb');
 
-gulp.task('default', function(){
+gulp.task('default', [ 'compile' ]);
+gulp.task('test', [ 'compile', 'tester' ]);
+
+//	compile is the main task that compiles each module
+gulp.task('compile',  function(){
 
 	var s = 'src/';
 
@@ -20,11 +25,24 @@ gulp.task('default', function(){
 
 });
 
-//	::TODO:: write a tester
-// gulp.task( 'test', [ 'default', function(){
-
-// 	gulp.src( 'dist/core.less' );
 
 
-// } ] );
 
+// //	::TODO:: write a tester
+gulp.task( 'tester', function(){
+
+
+	gulp.src( 'test/style.less' )
+		.pipe( less() )
+		.pipe( prfx({
+				browsers: ['last 2 versions'],
+            	cascade: true
+			    }) )
+				.on( 'error', function(e){
+					console.log("Ya done goofed!");
+					console.log(e);
+				} )
+		.pipe( comb() )
+		.pipe( gulp.dest( 'test/' ) );
+
+});
